47f0b65e9e3e352b3a65960825c41a0b78f6cc76
MiniDigger <admin@minidigger.me>
2021-02-23 19:10:53 +0100

    Add fallback piano, add websocket connection

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 var endpoint
+var websocketEndpoint
 var playerName
 var instrument
 var sendVelocity
+var websocket
 
 class Note {
-    constructor(instrument, pitch, velocity) {
-        this.instrument = instrument
+    constructor(instrumentValue, pitch, velocity) {
+        this.instrument = instrumentValue
         this.pitch = pitch
         this.velocity = velocity
     }
 }
 
 class NoteRequest {
-    constructor(playerName, note) {
-        this.playerName = playerName
+    constructor(playerNameValue, note, on) {
+        this.type = on ? "NoteOn" : "NoteOff"
+        this.playerName = playerNameValue
         this.note = note
     }
 }
@@ -28,9 +31,12 @@ function addText(id, value) {
 
 function onLoad() {
     endpoint = document.getElementById("endpoint")
+    websocketEndpoint = document.getElementById("websocket-endpoint")
     playerName = document.getElementById("player-name")
     instrument = document.getElementById("instrument")
     sendVelocity = document.getElementById("send-velocity")
+
+    setupKeys();
 
     WebMidi.enable(function (err) {
         if (err) {
@@ -54,35 +60,111 @@ function onLoad() {
             })
         }
     }, true)
+
+    connectWebsocket();
+    websocketEndpoint.onblur = function () {
+        connectWebsocket();
+    };
+}
+
+function connectWebsocket() {
+    console.log("connecting...");
+    websocket = new WebSocket(websocketEndpoint.value);
+    websocket.onopen = function () {
+        websocket.send(JSON.stringify({type: "Join", playerName: playerName.value}));
+    }
+    websocket.onmessage = function (event) {
+        console.log("got message from socket: " + event.data);
+    }
+    websocket.onerror = function (event) {
+        console.log("socket error: " + event.data);
+    }
+}
+
+function setupKeys() {
+    var keys = document.getElementsByClassName("piano-key");
+
+    for (let key of keys) {
+        var classes = key.classList;
+        if (!classes.contains("piano-disabled")) {
+            classes.forEach(function (c) {
+                if (c.startsWith("piano-key-")) {
+                    var keyName = c.replace("piano-key-", "");
+                    key.onclick = function () {
+                        onClick(keyName);
+                    }
+                }
+            })
+        }
+    }
 }
 
 function updateDeviceList() {
     setText("inputs", "Inputs: ")
-    for (input of WebMidi.inputs) {
+    for (var input of WebMidi.inputs) {
         addText("inputs", input.name + ", ")
     }
 }
 
 function addListeners() {
-    for (input of WebMidi.inputs) {
-        if (!input.hasListener("noteon", "all", noteOn)) {
-            input.addListener("noteon", "all", noteOn)
+    for (var input of WebMidi.inputs) {
+        if (!input.hasListener("noteon", "all", onNoteOn)) {
+            input.addListener("noteon", "all", onNoteOn)
+        }
+        if (!input.hasListener("noteoff", "all", onNoteOff)) {
+            input.addListener("noteon", "all", onNoteOff)
         }
     }
 }
 
-function noteOn(e) {
-    post(endpoint.value + "/note-on",
-        new NoteRequest(playerName.value,
-            new Note(instrument.value,
-                e.note.number,
-                sendVelocity.checked ? e.velocity : 1)))
+function onClick(keyName) {
+    var data = new NoteRequest(playerName.value,
+        new Note(instrument.value,
+            WebMidi.guessNoteNumber(keyName),
+            sendVelocity.checked ? e.velocity : 1),
+        true
+    );
+
+    sendData(data);
+}
+
+function onNoteOn(e) {
+    onNote(e, false);
+}
+
+function onNoteOff(e) {
+    onNote(e, false);
+}
+
+function onNote(e, on) {
+    var data = new NoteRequest(playerName.value,
+        new Note(instrument.value,
+            e.note.number,
+            sendVelocity.checked ? e.velocity : 1),
+        on
+    );
+    sendData(data)
+}
+
+function sendData(data) {
+    console.log("sendData: " + JSON.stringify(data));
+    post(endpoint.value + "/note", data);
+    sendMessage(websocketEndpoint.value, data);
 }
 
 function post(url, data) {
-    console.log(data)
-    console.log(JSON.stringify(data))
-    return fetch(url, { method: "POST", body: JSON.stringify(data) })
+    if (!url) {
+        return;
+    }
+    return fetch(url, {method: "POST", body: JSON.stringify(data)});
 }
 
-document.addEventListener("DOMContentLoaded", onLoad)
+function sendMessage(url, data) {
+    if (!url || !websocket || websocket.readyState !== WebSocket.OPEN) {
+        return;
+    }
+
+    websocket.send(JSON.stringify(data));
+}
+
+document.addEventListener("DOMContentLoaded", onLoad);
